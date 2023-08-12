8477edcd12d9f8724381bcfe80d7c0892176bdc0
Ashish Ujjwal <ashishfrommohania@gamil.com>
2023-08-12 12:29:08 +0530

    Cpp

---
+++ Operator.cpp
@@ -0,0 +1,23 @@
+// This Program is done by Ashish Ujjwal
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long int
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+#define pb push_back
+#define all(X) (X).begin(), (X).end()
+#define np cout << -1 << endl
+#define rn return
+
+int main(){
+
+    int n;cin>>n;
+    while(n--){
+        int x,y;
+        cin>>x>>y;
+        int add = x+y;
+        int sub = abs(x-y);
+    }
+    return 0;
+}
